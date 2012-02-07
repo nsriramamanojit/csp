e6dc14f7844e6d469c9fc52de609f1ffc03a4887
Srirama Manojit <nchaitanyaram@yahoo.com>
2012-02-07 15:43:37 +0530

    Logo

--- app/models/account.rb
+++ app/models/account.rb
@@ -1,9 +1,10 @@
 class Account < ActiveRecord::Base
 
+  #validations
   validates :name, :presence => true, :length => { :maximum => 200}
   validates :csp_code, :presence => true, :uniqueness => true, :length => { :maximum => 20}
 
-
+  #Search - Meta Where
     def self.search(search)
     if search
       where({:name.matches => "%#{search}%"} | {:csp_code.matches => "%#{search}%"})
